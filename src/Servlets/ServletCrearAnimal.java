4b3b317613d66504a5ecde680911927ccf9bd891
esteban027 <dbermudezb@unbosque.edu.co>
2015-11-23 14:02:44 -0500

    correcciones

--- src/Servlets/ServletCrearAnimal.java
+++ src/Servlets/ServletCrearAnimal.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class CrearAnimal
  */
-@WebServlet("CrearAnimal")
+@WebServlet("/ServletCrearAnimal")
 public class ServletCrearAnimal extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -29,6 +29,22 @@ public class ServletCrearAnimal extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	     String nombre = request.getParameter("nombre");
+	        String edad = request.getParameter("edad");
+	        String identificacion = request.getParameter("id");
+	        String descripcion = request.getParameter("des");
+	        String alimento = request.getParameter("alimento");
+	        Animal a = new Animal(nombre,identificacion,alimento,descripcion,edad);
+	        
+			response.setContentType("text/html");
+			PrintWriter pw = response.getWriter();
+			String docType =
+					"<!DOCTYPE html>\n";
+			pw.println(docType+"<HTML><HEAD><TITLE>Creacion de animales</TITLE></HEAD>"+"<BODY BGCOLOR=\"#CCBBAA\">"
+			+"<H2>Leyendo parametros desde un formulario html</H2><P>"+"<UL>\n"+"Verificacion de datos:"+
+			"El nombre del animal es: " +nombre  + "<BR>"+"La edad es: " + edad + "<BR>"+"Identificacion: " + identificacion + "<BR>"
+			+"El tipo de alimento que consume es: " +descripcion  + "<BR>"+"Descripcion del animal: " +alimento  + "</P><BR>"+"</BODY></HTML>");
+	
 	}
 
 	/**
@@ -36,20 +52,7 @@ public class ServletCrearAnimal extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// Obtenemos un objeto Print Writer para enviar respuesta
-		        String nombre = request.getParameter("nombre");
-		        String edad = request.getParameter("edad");
-		        String identificacion = request.getParameter("id");
-		        String descripcion = request.getParameter("des");
-		        String alimento = request.getParameter("alimento");
-		        Animal a = new Animal(nombre,identificacion,alimento,descripcion,edad);
-		        
-				response.setContentType("text/html");
-				PrintWriter pw = response.getWriter();
-				pw.println(response+"<HTML><HEAD><TITLE>Creacion de animales</TITLE></HEAD>"+"<BODY BGCOLOR=\"#CCBBAA\">"
-				+"<H2>Leyendo parametros desde un formulario html</H2><P>"+"<UL>\n"+"Verificacion de datos:"+
-				"El nombre del animal es: " +nombre  + "<BR>"+"La edad es: " + edad + "<BR>"+"Identificacion: " + identificacion + "<BR>"
-				+"El tipo de alimento que consume es: " +descripcion  + "<BR>"+"Descripcion del animal: " +alimento  + "<BR>"+"</BODY></HTML>");
-				
+		   		
 				
 			
 	}
