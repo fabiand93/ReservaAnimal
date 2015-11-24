f352b1350e0fdd9f2b4327a2bdcc93fd18794905
eospinam <osmoeda@gmail.com>
2015-11-24 00:02:11 -0500

    ServletModificarAnimal

---
+++ src/Controlador/ServletModificarAnimal.java
@@ -0,0 +1,41 @@
+package Controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletModificarAnimal
+ */
+@WebServlet("/ServletModificarAnimal")
+public class ServletModificarAnimal extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ServletModificarAnimal() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Reserva reserva = Reserva();
+		String identificacion= request.getParameter("identificacion");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
